52aa37a362c6d3a2390a83860aa19a5ece566c72
Pradeep Verma <pradeep.verma@tataaig.com>
2025-11-13 15:13:21 +0530

    sds

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: 'build',
   trailingSlash: false,
   assetPrefix: process.env.NODE_ENV === 'production' ? './' : undefined,
 };
